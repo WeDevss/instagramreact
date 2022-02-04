ceaa42c683952a7414ec8cbc6cf0e7a219fcf7e5
Doston-1097 <dostonbekbozorboyev7@gmail.com>
2022-02-04 18:03:47 +0500

    hello

---
+++ src/components/Right.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Right(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Right;
---
+++ src/components/left/Left.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Left(props) {
+    return (
+        <div className="bg-dark">
+
+        </div>
+    );
+}
+
+export default Left;
